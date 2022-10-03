8abb294443debcdddec3798b86d811167ba2d459
hyeonJeongByeon <jungjung1218@gmail.com>
2022-10-03 16:43:53 +0900

    final commit before interview

--- voice_django/settings/prod.py
+++ voice_django/settings/prod.py
@@ -2,4 +2,5 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['ec2-43-200-133-223.ap-northeast-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['ec2-3-39-23-78.ap-northeast-2.compute.amazonaws.com']
+#ALLOWED_HOSTS = ['ec2-43-200-133-223.ap-northeast-2.compute.amazonaws.com']
